574aa517ecf5d01cee43156bdc04b9647d8a8d52
bennewell32 <ben.m.newell@gmail.com>
2021-03-11 14:26:17 -0600

    rewrite for pickleball rules

--- apps/benWatch/benWatch.js
+++ apps/benWatch/benWatch.js
@@ -1,6 +1,9 @@
 var start = "false";
 var score1 = 0;
 var score2 = 0;
+var team1server = 0;
+var team2server = 0;
+var log = [];
 
 ///////////
 //ON LOAD//
@@ -9,7 +12,7 @@ g.clear(reset).setFont("6x8");
 g.setFont("Vector", 15).setFontAlign(0, 0);
 g.drawString("Pickleball", 130, 90);
 
-g.setFont("Vector", 40).setFontAlign(0, 0);
+g.setFont("Vector", 20).setFontAlign(0, 0);
 g.drawString("Score Keeper!", 130, 130);
 
 /////////
@@ -24,24 +27,61 @@ setWatch(
 );
 
 function pressBtn1() {
-  if (start === "false") {
-    start = "true";
-    score1 = 0;
-    score2 = 0;
-    g.clear(reset);
-    g.setFont("Vector", 40).setFontAlign(0, 0);
-    g.drawString("Started!", 130, 90);
-    g.drawString(score1 + ":" + score2, 130, 130);
-    Bangle.buzz();
-  } else {
-    score1 = score1 + 1;
-    g.clear(reset);
-    g.setFont("Vector", 40).setFontAlign(0, 0);
-    g.drawString("Team 1 Pt!", 130, 90);
-    g.drawString(score1 + ":" + score2, 130, 130);
-    Bangle.buzz();
+  if ((team1server === 0) & (team2server === 0)) {
+    setServer();
+  }
+  if (team1server === 1 || team2server === 1) {
+    if (start === "false") {
+      startGame();
+    }
+  }
+  if ((start = "true")) {
+    countPoint();
+    logRecord();
   }
 }
+
+function setServer() {
+  g.clear(reset);
+  g.setFont("Vector", 20).setFontAlign(0, 0);
+  g.drawString("SetServer", 130, 90);
+}
+
+function startGame() {
+  g.clear(reset);
+  g.setFont("Vector", 20).setFontAlign(0, 0);
+  g.drawString("Started", 130, 90);
+}
+
+function countPoint() {
+  g.clear(reset);
+  g.setFont("Vector", 20).setFontAlign(0, 0);
+  g.drawString("CountPoint and Logged!", 130, 90);
+}
+
+function logRecord() {
+  g.clear(reset);
+  g.setFont("Vector", 20).setFontAlign(0, 0);
+  g.drawString("CountPoint and Logged!", 130, 90);
+}
+
+// if (start === "false") {
+//   start = "true";
+//   score1 = 0;
+//   score2 = 0;
+//   g.clear(reset);
+//   g.setFont("Vector", 40).setFontAlign(0, 0);
+//   g.drawString("Started!", 130, 90);
+//   g.drawString(score1 + ":" + score2, 130, 130);
+//   Bangle.buzz();
+// } else {
+//   score1 = score1 + 1;
+//   g.clear(reset);
+//   g.setFont("Vector", 40).setFontAlign(0, 0);
+//   g.drawString("Team 1 Pt!", 130, 90);
+//   g.drawString(score1 + ":" + score2, 130, 130);
+//   Bangle.buzz();
+// }
 
 /////////
 //BTN 2//
